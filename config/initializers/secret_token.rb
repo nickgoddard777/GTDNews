f7aea04bd5a7a0a683a639e120a6db80eaf896a2
Nick Goddard <nicko777@gmail.com>
2011-11-23 23:23:17 +0000

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gtdnews::Application.config.secret_token = '2afcd87bb5a1f9cebbebbc4184a96a24bd256dd6cb7660838670da35ac40a4cc4921b0fa3c0deade3ec9f3da01eec4bd3e97103d87f6481fb0f2bf4b121c6fe6'
